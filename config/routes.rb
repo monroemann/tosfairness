7f57d07ee12b6f4819a7bb600bfcbecb62c40843
Monroe Mann <findingsummer@aol.com>
2018-01-09 00:00:58 +0000

    Fixed index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "websites#index"
+  
+  get "top_ten_fair", to: "websites#top_ten_fair"
+  get "top_ten_unfair", to: "websites#top_ten_fair"
+  get "contract_detail", to: "contracts#contract_detail"
+  get "contract_list", to: "contracts#contract_list"
 
 end
