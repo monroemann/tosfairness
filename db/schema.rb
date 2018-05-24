fbd10800cea535f2791e4dc0c075dee37665ccbe
Christian Van <khanhvan121992@gmail.com>
2018-05-24 01:18:12 +0000

    Runs migration for acts_as_votable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20180525012302) do
+=======
+ActiveRecord::Schema.define(version: 20180522083640) do
+>>>>>>> Runs migration for acts_as_votable
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
